b5654786969daf60501a6006c8e5994912aba045
Nic Martin <niciliketo@gmail.com>
2012-05-14 18:05:35 +0100

    fixes for mistakes in capitalization

--- lib/zohoho/lead.rb
+++ lib/zohoho/lead.rb
@@ -1,23 +1,13 @@
 module Zohoho
   class Lead < ZohoObject
-    @@myFields = [:company, :first_name, :last_name, :designation, :email, :phone, :fax, :mobile, :website, :lead_source, :lead_status, :industry, :no_of_employees, :annual_revenue, :email_opt_out, :skype_id, :salutation, :street, :city, :state, :zip_code, :country, :description, :annual_revenue, :rating, :industry_md]
-   attr_accessor *@@myFields
+    @@myFields = ["Company", "First Name", "Last Name", ":Designation", "Email", "Phone", "Fax", "Mobile", "Website", "Lead Source", "Lead Status", "Industry", "No of Employees",
+    "Annual Revenue", "Email Opt Out", "Skype ID", "Salutation", "Street", "City", "State", "Zip Code", "Country", "Description", "Annual Revenue", "Rating"]
+   #attr_accessor *@@myFields
 
     def initialize(h={})
       @@myFields.each do |f|
         self[f] = h[f] ||''
         end
-    end
-    protected
-    def zohoizeField(sym)
-      if sym == :no_of_employees
-        r = "No of Employees"
-      elsif sym == :skype_id
-        r = "Skype ID"
-      else
-        r = super(sym)
-      end
-      r
     end
   end
 end
--- lib/zohoho/zoho_object.rb
+++ lib/zohoho/zoho_object.rb
@@ -8,30 +8,13 @@ module Zohoho
       self.class.to_s.split('::').last.pluralize || ''
     end
 
-    def to_s
-      sync_symbols_and_methods
-      super
-    end
+
     protected
     def parse_data
-      sync_symbols_and_methods
-      fl = self.map {|e| Hash['val', zohoizeField(e[0]), 'content', (e[1]||'')]}
+      fl = self.map {|e| Hash['val', (e[0]), 'content', (e[1]||'')]}
       row = Hash['no', '1', 'FL', fl]
       data = Hash['row', row]
       XmlSimple.xml_out(data, :RootName => data_name)
-    end
-    def zohoizeField(sym)
-      r = sym
-      #We will onyl mess with ones we know as methods, rely on the right format for any custom fields defined in the hash
-      if self.send(sym)
-        r=sym.to_s.gsub('_',' ').titleize
-      end
-      return r
-    end
-    def sync_symbols_and_methods
-      # Wanted to give methods to set common attributes, but also flexibility to set a symbol too
-      # We actually iterate the symbols, so this is a chacne to sync the two
-      self.map {|e| if (self.send(e[0])) then self[e[0]] = self.send(e[0]) end }
     end
   end
 end
