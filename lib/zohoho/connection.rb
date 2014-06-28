3003b171eaf386b92978253fe52a8507b70128b3
Nicholas Martin <nicholas.martin@marketdojo.com>
2014-06-28 15:37:26 +0100

    Dont swallow errors returned from Zoho, raise exceptions instead.

--- lib/zohoho/connection.rb
+++ lib/zohoho/connection.rb
@@ -17,42 +17,59 @@ module Zohoho
 
     def call(entry, api_method, query = {}, http_method = :get)
       query.merge!({ :authtoken => self.auth_token, :scope => "#{@service_name.downcase}api" })
-
       url = [zoho_uri, entry, api_method].join('/')
       case http_method
       when :get
         res = self.class.get(url, :query => query).parsed_response
         res = res.to_json if res.kind_of?(Hash)
         raw = JSON.parse(res)
-        parse_raw_get(raw, entry)
+        res = parse_raw_get(raw, entry)
       when :post
         res = self.class.post(url, :body => query)
         res = res.to_json if res.kind_of?(Hash)
         raw = JSON.parse(res)
-        parse_raw_post(raw)
+        res = parse_raw_post(raw)
       else
         raise "#{http_method} is not a recognized http method"
       end
+      if ((res.class == Hash) && res['error'])
+        parse_error(res, query)
+      end 
+      return res
     end
 
     private
 
     def parse_raw_get(raw, entry)
-      return [] if raw['response']['result'].nil?
-      rows = raw['response']['result'][entry]['row']
-      rows = [rows] unless rows.class == Array
-      rows.map {|i|
-        raw_to_hash i['FL']
-      }
+      if raw['response']['result'].nil?
+        return_value =  [] 
+      elsif !raw['response']['error'].nil?
+        return_value = raw['response'] 
+      else
+        rows = raw['response']['result'][entry]['row']
+        rows = [rows] unless rows.class == Array
+        return_value = rows.map {|i|
+          raw_to_hash i['FL']
+        }
+      end
+      return return_value
     end
 
     def parse_raw_post(raw)
-      if raw['response']['result'].nil?
-        return Hash[raw]['response']['error']
+      if !raw['response']['error'].nil?
+        return_value = raw['response']
       else
         record = raw['response']['result']['recorddetail']
-        raw_to_hash record['FL']
+        return_value = raw_to_hash record['FL']
       end
+      return return_value
+    end
+
+    def parse_error(response, query)
+      error =  "Zoho returned error code #{response['error']['code']} : #{response['error']['message']}.\n" +
+              "The query sent to Zoho was: #{query}."
+      puts error
+      raise error
     end
 
 
