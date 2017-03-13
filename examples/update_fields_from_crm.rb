f4ba124a475998dff5d0aea2b5529318de8dadf7
Nic Martin <nicholas.martin@marketdojo.com>
2017-03-13 13:05:06 +0000

    Make script more robust

--- examples/update_fields_from_crm.rb
+++ examples/update_fields_from_crm.rb
@@ -13,17 +13,20 @@ require 'csv'
 
 # ADD API KEY HERE (but dont commit to github)
 crm_auth_token = ''
-print 'CRM Auth Token:'
+records_to_process = 4
+print "ONLY PROCESSING THE FIRST #{records_to_process} RECORDS - AMEND THE SCRIPT WHEN YOU ARE SURE IT WORKS CORRECTLY"
 if crm_auth_token.empty?
   puts 'please add crm_auth_token key'
   exit
 end
+puts "CRM Auth Token: #{crm_auth_token}"
+
 # An empty hask (key&value pairs) to hold our update
 # connect to CRM
 @crm = Zohoho::Crm.new(crm_auth_token)
 
 # Load the CSV
-CSV.foreach('eWorld.csv', :headers => true) do |row|
+CSV.foreach('examples/eWorld.csv', :headers => true) do |row|
   # We expect each row to contain several values, which we can access as follows:-
   # row['NAME']
   # row['Company']
@@ -44,14 +47,14 @@ CSV.foreach('eWorld.csv', :headers => true) do |row|
   # there are lots of ways to do this, including...
   contact_id = row['Zoho Contact ID'].gsub('zcrm_', '')
 
-  if ($.).to_i < 4
+  if ($.).to_i <= records_to_process
     #crm_data = @crm.contact_with_id(contact_id)
     if contact_id.length > 8
     #pp crm_data
-    contact = Zohoho::Contact.new
-    contact['Temp Field 2'] = row['Data tag (Temp field)']
-    @crm.update_object(contact_id, contact)
-    pp @crm
+      contact = Zohoho::Contact.new
+      contact['Temp Field 2'] = row['Data tag (Temp field)']
+      res = @crm.update_object(contact_id, contact)
+      puts "Updated #{row['NAME']}"
     else
       puts "Not attempted, invalid contact id #{contact_id}"
     end
