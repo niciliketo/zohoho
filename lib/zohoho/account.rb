a2e9f619f177f0f5de573952cc3ac9bac690e7f8
Xymist <james.duerden@marketdojo.com>
2018-11-27 19:42:35 +0000

    Do not discard custom elements on initializing contacts

--- lib/zohoho/account.rb
+++ lib/zohoho/account.rb
@@ -1,11 +1,22 @@
-  module Zohoho
+module Zohoho
+  # An account represents a paying customer in the CRM, generally a company.
   class Account < ZohoObject
-    @@myFields = ["ACCOUNTID", "Account Name", "Phone", "Fax", "Employees", "Billing Street", "Shipping Street", "Billing City", "Shipping City", "Billing State", "Shipping State", "Billing Code", "Shipping Code", "Billing Country","Shipping Country", "Description", "Agreement", "Description", "Website", "Annual Revenue" ]
+    DEFAULT_FIELDS = [
+      "ACCOUNTID", "Account Name", "Phone", "Fax", "Employees",
+      "Billing Street", "Shipping Street", "Billing City", "Shipping City",
+      "Billing State", "Shipping State", "Billing Code", "Shipping Code",
+      "Billing Country","Shipping Country", "Description", "Agreement",
+      "Description", "Website", "Annual Revenue"
+    ]
 
-    def initialize(h={})
-      @@myFields.each do |f|
-        self[f] = h[f] ||''
-        end
+    def initialize(elements = {})
+      # The default fields are expected by Zoho
+      DEFAULT_FIELDS.each do |field|
+        self[field] = elements.delete(field) || ''
+      end
+      # It is possible to pass arbitrary additional elements to an account,
+      # which will populate custom fields.
+      elements.each { |key, value| self[key] = value } unless elements.empty?
     end
   end
 end
