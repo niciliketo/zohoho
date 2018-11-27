24d500946096b44a2e625503b202a7a9c327d6c5
Xymist <james.duerden@marketdojo.com>
2018-11-27 19:45:26 +0000

    Extend to Accounts and Leads

--- lib/zohoho/lead.rb
+++ lib/zohoho/lead.rb
@@ -1,13 +1,21 @@
 module Zohoho
   class Lead < ZohoObject
-    @@myFields = ["Company", "First Name", "Last Name", ":Designation", "Email", "Phone", "Fax", "Mobile", "Website", "Lead Source", "Lead Status", "Industry", "No of Employees",
-    "Annual Revenue", "Email Opt Out", "Skype ID", "Salutation", "Street", "City", "State", "Zip Code", "Country", "Description", "Annual Revenue", "Rating"]
-   #attr_accessor *@@myFields
+    DEFAULT_FIELDS = [
+      "Company", "First Name", "Last Name", ":Designation", "Email", "Phone",
+      "Fax", "Mobile", "Website", "Lead Source", "Lead Status", "Industry",
+      "No of Employees", "Annual Revenue", "Email Opt Out", "Skype ID",
+      "Salutation", "Street", "City", "State", "Zip Code", "Country",
+      "Description", "Annual Revenue", "Rating"
+    ]
 
-    def initialize(h={})
-      @@myFields.each do |f|
-        self[f] = h[f] ||''
-        end
+    def initialize(elements = {})
+      # The default fields are expected by Zoho
+      DEFAULT_FIELDS.each do |field|
+        self[field] = elements.delete(field) || ''
+      end
+      # It is possible to pass arbitrary additional elements to a lead,
+      # which will populate custom fields.
+      elements.each { |key, value| self[key] = value } unless elements.empty?
     end
   end
 end
